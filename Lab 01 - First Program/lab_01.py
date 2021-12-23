94288df8b1fed3472f1628223ac997c7f64d3180
TheSpy0 <TheSpy0@gmail.com>
2021-12-22 19:31:26 -0800

    created four print statements.

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,3 +1,8 @@
-print("Hello. This program simply prints out a string to standard out.")
-
+print("Hello.")
+print("This program simply prints out a string to standard output.")
 print("Sometimes you just \nwant to put\ntext on multiple\nlines.")
+print("""This is my attempt
+at putting multiple lines
+into one print statement
+using triple double-quotes.
+""")
